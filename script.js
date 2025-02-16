636baa250feffb516485792e41ca27d07b0b3a97
Ashwini-123523 <73285811+Ashwini-123523@users.noreply.github.com>
2025-02-15 22:35:32 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const roles = ["Data Scientist", "Data Engineer", "Data Analyst"];
+let index = 0;
+
+function changeText() {
+    let textElement = document.getElementById("changing-text");
+    
+    // Fade out
+    textElement.style.opacity = 0;
+    
+    setTimeout(() => {
+        // Change text
+        textElement.textContent = roles[index];
+        index = (index + 1) % roles.length;
+        
+        // Fade in
+        textElement.style.opacity = 1;
+    }, 500); // Matches CSS transition time
+}
+
+// Change every 2 seconds
+setInterval(changeText, 2000);
